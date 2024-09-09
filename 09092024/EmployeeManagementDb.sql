88f93038ed6c1bcbbbf56b1501eecf4f1393de95
gmaheswaranmca <gmaheswaranmca@gmail.com>
2024-09-09 15:58:46 +0530

    DAy 01 learnings file uploaded

--- 09092024/EmployeeManagementDb.sql
+++ 09092024/EmployeeManagementDb.sql
@@ -56,5 +56,13 @@ DELETE FROM Person
 WHERE id=5;
 
 
-
-
+-- departments {id, name}
+CREATE TABLE departments
+(
+    id INT PRIMARY KEY auto_increment,
+    name VARCHAR(100) NOT NULL
+);
+DESC departments;
+SELECT * FROM departments;
+INSERT INTO departments(name) 
+VALUES ('Analysis'),('Design'),('Development'),('Testing');
