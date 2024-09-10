d530585e4d7f518818f92a3cc25b7b9d6e7e56b7
gmaheswaranmca <gmaheswaranmca@gmail.com>
2024-09-10 15:49:44 +0530

    day 02 - django notes

--- 09102024/mysql-handson-employee.sql
+++ 09102024/mysql-handson-employee.sql
@@ -148,3 +148,113 @@ INSERT INTO assignments (employee_id, project_id, role) VALUES
         (9, 5, 'Systems Engineer'),
         (10, 5, 'Database Analyst');
 	SELECT * FROM assignments;
+
+-- columns selectors | filtering | sorting | pagination 
+-- to query the bob based on full name (equals)
+SELECT * FROM employees WHERE name='Bob Brown';
+
+-- to query the bob based name starts with 
+SELECT * FROM employees WHERE name LIKE 'Bob%';
+
+-- to query the bob based name end with 
+SELECT * FROM employees WHERE name LIKE '%Brown';
+
+-- to query all rows by columns selectors 
+SELECT id, name, dept_id, job_title, salary, bonus from Employees;
+
+-- to display name, salary and bonus for all employees 
+SELECT name, salary, bonus FROM employees;
+
+-- to display name, salary, bonus, total_salary for all employees 
+-- SELECT name, salary, bonus, salary + bonus  FROM employees;
+SELECT name, salary, bonus, salary + bonus as total_salary  FROM employees;
+
+-- operators + - * / % POW
+
+--  to display name, bonus, change_percentage, changed_bonus for all employees 
+SELECT name, bonus, 15 as change_percentage, bonus / 100.0 * 115.0 as changed_bonus from employees;
+SELECT name, bonus, 15 as change_percentage, bonus * 1.15 as changed_bonus from employees;
+SELECT name, bonus, 15 as change_percentage, 
+	(bonus + (bonus / 100.0 * 15.0)) as changed_bonus from employees;
+    
+-- filtering | conditional query | WHERE condition 
+	-- comparison operators 	=   !=  <>    <   >   <= >= 
+	-- logical operators 	    AND OR NOT 
+-- to display employees whose salary is above 70000
+SELECT * FROM employees WHERE salary > 70000;
+
+-- to display employees whose salary is on or below 70000
+SELECT * FROM employees WHERE salary <= 70000;
+
+-- to display employees whose salary is either 65000 or 75000
+SELECT * FROM employees WHERE salary = 65000 OR salary = 75000;
+
+SELECT * FROM employees WHERE salary IN (65000, 75000);
+
+-- to display employees whose salary is neither 65000 nor 75000
+SELECT * FROM employees WHERE NOT (salary = 65000 OR salary = 75000);
+
+SELECT * FROM employees WHERE salary NOT IN (65000, 75000);
+
+-- to display employees whose salary is between 60000 and 80000
+SELECT * FROM employees WHERE salary>=60000 AND salary<=80000;
+
+SELECT * FROM employees WHERE salary BETWEEN 60000 AND 80000;
+-- to display employees whose salary is not between 60000 and 80000
+SELECT * FROM employees WHERE salary NOT BETWEEN 60000 AND 80000;
+
+-- sort employees based on name a-z order 
+SELECT * FROM employees ORDER BY name; -- ASC is optional here
+SELECT * FROM employees ORDER BY name ASC;
+-- sort employees based on name z-a order 
+SELECT * FROM employees ORDER BY name DESC;
+-- sort employees based on salary ascending 
+SELECT * FROM employees ORDER BY salary;
+-- sort employees based on salary descending 
+SELECT * FROM employees ORDER BY salary DESC;
+-- top 3 salaried
+SELECT * FROM employees ORDER BY salary DESC LIMIT 3;
+-- 4th top salaried 3 rows
+SELECT * FROM employees ORDER BY salary DESC LIMIT 3 OFFSET 3;
+
+-- LIMIT and OFFSET are used for pagination 
+	-- LIMIT defines rows count to be seleced 
+	-- OFFSET defines rows count to be skipped 
+    -- SELECT * FROM Google LIMIT 10; -- page 1, here 10 is num of rows displayed
+    -- SELECT * FROM Google LIMIT 10 OFFSET 10; -- page 2
+    -- SELECT * FROM Google LIMIT 10 OFFSET 10*2; -- page 3
+    -- SELECT * FROM Google LIMIT 10 OFFSET 10*15; -- page 15
+
+
+
+-- to display employees name, bonus, change_percentage, changed_bonus
+-- and sort employees based on changed bonus ascending order 
+
+SELECT name, bonus, 
+    15 as change_percentage, 
+    bonus * 1.15  
+from employees
+ORDER BY 4; -- ORDINAL NUMBER BASED SORTING
+
+-- to display employees name, bonus, change_percentage, changed_bonus
+-- and sort employees based on changed bonus descending order 
+
+SELECT name, bonus, 
+    15 as change_percentage, 
+    bonus * 1.15  
+from employees
+ORDER BY 4 DESC; -- ORDINAL NUMBER BASED SORTING
+
+
+-- to display employees name, bonus, change_percentage, changed_bonus
+-- and sort employees based on changed bonus descending order 
+SELECT name, bonus, 
+    15 as change_percentage, 
+    bonus * 1.15  as changed_bonus
+from employees
+ORDER BY changed_bonus DESC; -- sort by alias name of the column
+
+
+-- sort by dept_id descending then by name ascending
+SELECT * FROM employees ORDER BY dept_id desc, name asc;
+
