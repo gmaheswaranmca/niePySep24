7717288fda83576ed60724baa4e68e67d9c755b5
gmaheswaranmca <gmaheswaranmca@gmail.com>
2024-09-10 10:59:11 +0530

    tables departments employees projects assignments created

--- 09102024/mysql-handson-employee.sql
+++ 09102024/mysql-handson-employee.sql
@@ -52,3 +52,56 @@ USE EmployeeManagementDb;
             DROP TABLE IF EXISTS departments;
     -- DROP DATABASE EmployeeManagementDb;
     DROP DATABASE IF EXISTS EmployeeManagementDb;
+
+
+
+    -- MySQL Learning continued 
+
+-- drop db if there, otherwise create db 
+Drop Database IF EXISTS EmployeeManagementDb;
+Create Database IF NOT EXISTS EmployeeManagementDb;
+USE EmployeeManagementDb;
+-- departments, employees, projects, assignments
+-- departments{id, name}
+DROP TABLE IF EXISTS departments;
+CREATE TABLE IF NOT EXISTS departments (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+	name VARCHAR(255) NOT NULL
+);
+DESC departments;
+SELECT * FROM departments;
+-- employees{id, name, dept_id, job_title, salary, bonus}
+DROP TABLE IF EXISTS employees;
+CREATE TABLE IF NOT EXISTS employees
+(
+	id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(255) NOT NULL,
+    dept_id INT NOT NULL,
+    job_title VARCHAR(100) NOT NULL,
+    salary FLOAT NOT NULL,
+    bonus FLOAT NOT NULL,
+    FOREIGN KEY(dept_id) REFERENCES departments(id)    
+);
+DESC employees;
+SELECT * FROM employees;
+-- projects{id, name, budget}
+DROP TABLE IF EXISTS projects;
+CREATE TABLE IF NOT EXISTS projects (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+	name VARCHAR(255) NOT NULL,
+	budget FLOAT NOT NULL
+);
+DESC projects;
+SELECT * FROM projects;
+-- assignments{id, employee_id, project_id, role}
+DROP TABLE IF EXISTS assignments;
+CREATE TABLE IF NOT EXISTS assignments (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+	employee_id INT NOT NULL,
+    project_id INT NOT NULL,
+    role VARCHAR(255) NOT NULL,
+    FOREIGN KEY(employee_id) REFERENCES employees(id),
+    FOREIGN KEY(project_id) REFERENCES projects(id)
+);
+DESC assignments;
+SELECT * FROM assignments;
