af27a41f94c80dd9f5f02808989871a889f09995
gmaheswaranmca <gmaheswaranmca@gmail.com>
2024-09-12 15:02:59 +0530

    ClinicalProject DoctorApp crud operations

--- ClinicalProject/ClinicalProject/urls.py
+++ ClinicalProject/ClinicalProject/urls.py
@@ -21,6 +21,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('doctors/', view=views.list_of_doctors, name='list_of_doctors'),
     path('doctors/create/', view=views.create_doctor, name='create_doctor'),
-    path('doctors/edit/<id>', view=views.edit_doctor, name='edit_doctor'),
-    path('doctors/delete/<id>', view=views.delete_doctor, name='delete_doctor'),
+    path('doctors/edit/<id>/', view=views.edit_doctor, name='edit_doctor'),
+    path('doctors/delete/<id>/', view=views.delete_doctor, name='delete_doctor'),
 ]
