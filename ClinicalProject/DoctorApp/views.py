7a0e3031336b52adcffc4e91e9220b3977844605
gmaheswaranmca <gmaheswaranmca@gmail.com>
2024-09-13 10:50:25 +0530

    ClinicalProject DoctorApp crud operations

--- ClinicalProject/DoctorApp/views.py
+++ ClinicalProject/DoctorApp/views.py
@@ -6,33 +6,31 @@ def list_of_doctors(request):
     return render(request, 'list.html', {'doctors' : doctors}) ######
 
 def create_doctor(request):
-    return render(request, 'form.html')
-    #GET method
-        #connect to the template "form.html" 
-    #POST method 
-        # name 
-        # specialization
-        # year_of_experience
-        # doctor = Doctor(name = name, specialization = specialization, year_of_experience = year_of_experience)
-        # doctor.save()
-        #redirect list page 
+    if request.method == 'GET':
+        return render(request, 'create.html')
+    elif request.method == 'POST':
+        name = request.POST['name'] 
+        specialization = request.POST['specialization'] 
+        year_of_experience = request.POST['year_of_experience'] 
+        doctor = Doctor(name = name, specialization = specialization, year_of_experience = year_of_experience)
+        doctor.save()
+        return redirect('list_of_doctors')         
 
 def edit_doctor(request, id):
-    return render(request, 'form.html')
-    #GET method
-        # doctor = Doctor.objects.get(id = id)
-        #connect to the template "form.html" pass doctor as param  
-    #POST method 
-        # doctor = Doctor.objects.get(id = id)
-        # doctor.name =  
-        # doctor.specialization
-        # doctor.year_of_experience
-        # doctor.save()
-        #redirect list page 
+    if request.method == 'GET':
+        doctor = Doctor.objects.get(id = id)
+        return render(request, 'edit.html', {'doctor' :doctor})
+    elif request.method == 'POST':
+        doctor = Doctor.objects.get(id = id)
+        doctor.name = request.POST['name'] 
+        doctor.specialization = request.POST['specialization'] 
+        doctor.year_of_experience = request.POST['year_of_experience'] 
+        doctor.save()
+        return redirect('list_of_doctors')    
+
         
 def delete_doctor(request, id):
+    doctor = Doctor.objects.get(id = id)
+    doctor.delete()
     return redirect('list_of_doctors')
-    # doctor = Doctor.objects.get(id = id)
-    # doctor.delete()
-    #redirect list page 
 
